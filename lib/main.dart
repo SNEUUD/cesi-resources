d4aaa685b9db8a0b44848bd15d9e8b395512b82e
ಠ_ಠ <christopher.crepin.72000@gmail.com>
2025-04-10 19:35:56 +0200

    added login and register page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'view/auth/login_view.dart';
+import 'view/auth/register_view.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +11,48 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World! LE CESI !!!'),
+    return MaterialApp(home: const HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Align(
+          alignment: Alignment.topRight,
+          child: Row(
+            mainAxisAlignment:
+                MainAxisAlignment
+                    .start, 
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LoginPage()),
+                  );
+                },
+                child: const Text('Se connecter'),
+              ),
+              const SizedBox(width: 20), 
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const RegisterPage(),
+                    ),
+                  );
+                },
+                child: const Text("S'inscrire"),
+              ),
+            ],
+          ),
         ),
       ),
     );
