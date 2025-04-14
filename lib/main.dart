ed313c190e3ad66342060ca325ff8887b07bde3c
ಠ_ಠ <christopher.crepin.72000@gmail.com>
2025-04-14 11:34:25 +0200

    backend init

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 import 'view/layout/header.dart';
 
 void main() {
@@ -14,14 +17,54 @@ class MainApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<dynamic> users = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUsers();
+  }
+
+  Future<void> fetchUsers() async {
+    final response = await http.get(Uri.parse('http://localhost:3000/users'));
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      setState(() {
+        users = data;
+        isLoading = false;
+      });
+    } else {
+      throw Exception('Failed to load users');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const Header(),
-      body: Center(child: Text('Hello World! LE CESI !!! Je fais un test')),
+      body:
+          isLoading
+              ? const Center(child: CircularProgressIndicator())
+              : ListView.builder(
+                itemCount: users.length,
+                itemBuilder: (context, index) {
+                  final user = users[index];
+                  return ListTile(
+                    title: Text(user['nomUtilisateur'] ?? 'Sans nom'),
+                    subtitle: Text(user['email'] ?? ''),
+                  );
+                },
+              ),
     );
   }
 }
