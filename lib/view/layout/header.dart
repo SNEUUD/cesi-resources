f79be724e90d3905a8dd90ab88821d23d271f573
ಠ_ಠ <christopher.crepin.72000@gmail.com>
2025-04-14 09:19:45 +0200

    fix

---
+++ lib/view/layout/header.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import '../auth/login_view.dart';
+import '../auth/register_view.dart';
+
+class Header extends StatelessWidget implements PreferredSizeWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.blue,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: const [Text('(RE)SOURCES')],
+      ),
+      actions: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LoginPage()),
+                );
+              },
+              child: const Text('Se connecter'),
+            ),
+            const SizedBox(width: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const RegisterPage()),
+                );
+              },
+              child: const Text("S'inscrire"),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
