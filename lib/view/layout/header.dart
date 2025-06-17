0060703b5111a8dc76e44d61ec26b3e547e4c3d0
ಠ_ಠ <christopher.crepin.72000@gmail.com>
2025-06-17 09:17:43 +0200

    fix

--- lib/view/layout/header.dart
+++ lib/view/layout/header.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:resources/view/profile_view.dart';
 import '../auth/login_view.dart';
-import '../auth/register_view.dart';
 import '../categories_view.dart';
 import '../create_resource_view.dart';
 import '../all_resources_view.dart'; // <-- à créer si besoin
