af8f172c5176511a4889f984ac171f3b79090728
el351 <elio.desoeuvre@viacesi.fr>
2025-09-29 09:33:22 +0200

    CI.CD test

--- lib/view/page_accueil.dart
+++ lib/view/page_accueil.dart
@@ -121,7 +121,7 @@ class _HomePageState extends State<HomePage> {
                       padding:
                           const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
                     ),
-                    child: const Text("Se connecter / S'inscrire"),
+                    child: const Text("Se connecter / s'inscrire"),
                   ),
                 ],
               ),
